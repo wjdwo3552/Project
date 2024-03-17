12e0c19ce4b0923aafaa0eccbf7e9ed9ecebc81e
maeng jung jae <a01093019627@gmail.com>
2024-03-17 22:12:52 +0900

    물고기 길이와 무게

---
+++ Main.py
@@ -0,0 +1,4 @@
+from Module2 import retrieve_data
+
+data = retrieve_data()
+print(data)
---
+++ Module2.py
@@ -0,0 +1,36 @@
+from Module1 import connect_to_mysql, execute_query, close_connection
+import schedule
+import time
+
+def retrieve_and_save_data():
+    data = retrieve_data()
+    print("데이터 저장됨.")
+
+def retrieve_data():
+    conn, cursor = connect_to_mysql(
+        host='121.155.34.16',
+        port=33063,
+        user='sysop',
+        password='data001!',
+        database='fms'
+    )
+
+    query = "SELECT * FROM growth_day_tb"
+    result = execute_query(cursor, query)
+
+    data_list = []
+
+    for row in result:
+        data_list.append(row)
+
+    close_connection(conn, cursor)
+
+    return data_list
+
+if __name__ == "__main__":
+    retrieve_and_save_data()
+    schedule.every(30).minutes.do(retrieve_and_save_data)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
